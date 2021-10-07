95a7d5e00829a1ac96baf09a4c833c756d44263a
Subramaniam Sahoo <subsahoo@adobe.com>
2021-10-07 11:49:17 +0530

    Basic skeleton for calculator class and design

---
+++ scripts/calculator.js
@@ -0,0 +1,24 @@
+class Calculator {
+    
+  
+    clear() {
+      this.currentCalString = ''
+    }
+  
+    delete() {
+      this.currentCalString = this.currentCalString.toString().slice(0, -1)
+    }
+  
+  
+    calculate() {
+     
+    }
+  
+    getDisplay() {
+      
+    }
+  
+    updateDisplay() {
+      
+    }
+  }
