f08715c81ce3f0c7688644fdfebc81743798bd56
Subramaniam Sahoo <subsahoo@adobe.com>
2021-10-07 12:49:15 +0530

    Binding the innerText string to Calculator screen

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,24 +1,68 @@
 class Calculator {
-    
-  
+
+
+    constructor(screen) {
+        this.screen = screen;
+    }
+
     clear() {
-      this.currentCalString = ''
+        this.currentCalString = '';
     }
-  
+
     delete() {
-      this.currentCalString = this.currentCalString.toString().slice(0, -1)
+        this.currentCalString = this.currentCalString.toString().slice(0, -1)
     }
-  
-  
+
+
     calculate() {
-     
+
     }
-  
+
     getDisplay() {
-      
+
     }
-  
-    updateDisplay() {
-      
+
+    updateDisplay(innerText) {
+        this.screen.innerText += innerText;
+
     }
-  }
+}
+
+
+(function () {
+    let screen = document.querySelector(".cal-screen");
+
+    let calculator = new Calculator(screen);
+
+    document.querySelectorAll(".operation").forEach(btn => {
+        btn.addEventListener('click', () => {
+            alert(btn.innerText)
+        })
+    });
+
+    document.querySelectorAll(".operand").forEach(btn => {
+        btn.addEventListener('click', () => {
+            calculator.updateDisplay(btn.innerText)
+        })
+    });
+
+
+    document.querySelectorAll(".number").forEach(btn => {
+        btn.addEventListener('click', () => {
+            calculator.updateDisplay(btn.innerText)
+        })
+    });
+
+    document.querySelectorAll(".dot").forEach(btn => {
+        btn.addEventListener('click', () => {
+            calculator.updateDisplay(btn.innerText)
+        })
+    });
+
+    document.querySelectorAll(".calculate").forEach(btn => {
+        btn.addEventListener('click', () => {
+            alert(btn.innerText)
+        })
+    });
+
+}())
