087d7703f1392a45b1c3eed9d512d93e2cb4a252
Subramaniam Sahoo <subsahoo@adobe.com>
2021-10-07 23:04:08 +0530

    adding calculate logic

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,30 +1,84 @@
 class Calculator {
-
-
     constructor(screen) {
         this.screen = screen;
+        this.displayNumArray = [];
+        this.operators = new Set(['/', '*', '+', '-']);
+        this.prevDigit = '';
+        this.resultDisplayed = false;
     }
 
     clear() {
-        this.currentCalString = '';
+        this.screen.innerText = '';
+        this.displayNumArray = [];
+        this.prevDigit = '';
+        this.resultDisplayed = false;
     }
 
     delete() {
-        this.currentCalString = this.currentCalString.toString().slice(0, -1)
+        this.screen.innerText = this.screen.innerText.slice(0, -1)
+        this.prevDigit = this.prevDigit.slice(0, -1)
     }
 
 
     calculate() {
 
+        let stack = [];
+        let num = '';
+        let sign = null
+
+        for (let i = 0; i <= this.displayNumArray.length; i++) {
+            const currNum = this.displayNumArray[i];
+
+            if (!isNaN(currNum)) num += currNum;
+
+            if (isNaN(currNum)) {
+                num = Number(num)
+                switch (sign) {
+                    case '+':
+                    case null:
+                        stack.push(num)
+                        break;
+                    case '-':
+                        stack.push(-num)
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num)
+                        break;
+                    case '/':
+                        stack.push(stack.pop() / num)
+                        break;
+                }
+                sign = currNum;
+                num = '';
+            }
+        }
+
+        return stack.reduce((a, b) => {
+            return a + b
+        }, 0)
     }
 
-    getDisplay() {
-
+    displayResult() {
+        this.displayNumArray.push(this.prevDigit);
+        this.screen.innerText = this.calculate();
+        this.resultDisplayed = true;
+        this.displayNumArray = [];
     }
 
     updateDisplay(innerText) {
+        if (innerText === '.' && this.prevDigit.includes('.')) return
+        
+        if (this.resultDisplayed) {
+            this.clear();
+        }
         this.screen.innerText += innerText;
-
+        if (this.operators.has(innerText)) {
+            this.displayNumArray.push(this.prevDigit);
+            this.displayNumArray.push(innerText);
+            this.prevDigit = '';
+        } else {
+            this.prevDigit += innerText;
+        }
     }
 }
 
@@ -36,7 +90,11 @@ class Calculator {
 
     document.querySelectorAll(".operation").forEach(btn => {
         btn.addEventListener('click', () => {
-            alert(btn.innerText)
+            if(btn.innerText==='CE'){
+                calculator.clear();
+            }else if(btn.innerText==='DEL'){
+                calculator.delete();
+            }
         })
     });
 
@@ -53,16 +111,16 @@ class Calculator {
         })
     });
 
-    document.querySelectorAll(".dot").forEach(btn => {
-        btn.addEventListener('click', () => {
-            calculator.updateDisplay(btn.innerText)
-        })
+    let dotBtn = document.querySelector(".dot");
+    dotBtn.addEventListener('click', () => {
+        calculator.updateDisplay(dotBtn.innerText)
     });
 
-    document.querySelectorAll(".calculate").forEach(btn => {
-        btn.addEventListener('click', () => {
-            alert(btn.innerText)
-        })
+
+    let calBtn = document.querySelector(".calculate");
+    calBtn.addEventListener('click', () => {
+        calculator.displayResult();
     });
+
 
 }())
